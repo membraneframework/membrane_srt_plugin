6de7df31f1ab157f8b57a5ef02e26421b237f8f3
Łukasz Kita <lukasz.kita0@gmail.com>
2025-04-23 12:14:22 +0200

    Add examples

---
+++ examples/receiver.exs
@@ -0,0 +1,58 @@
+defmodule ReceivingPipeline do
+  use Membrane.Pipeline
+
+  @impl true
+  def handle_init(_ctx, opts) do
+    spec =
+      child(%Membrane.SRT.Source{port: opts[:port]})
+      |> child(:demuxer, Membrane.MPEG.TS.Demuxer)
+
+    {[spec: spec], %{}}
+  end
+
+  @impl true
+  def handle_child_notification({:mpeg_ts_pmt, pmt}, :demuxer, _context, state) do
+    streams_spec =
+      Enum.map(pmt.streams, fn {id, %{stream_type: type}} ->
+        get_child(:demuxer)
+        |> via_out(Pad.ref(:output, {:stream_id, id}))
+        |> then(
+          &case type do
+            :H264 ->
+              &1 |> child(%Membrane.H264.Parser{output_stream_structure: :avc1})
+
+            :AAC ->
+              &1 |> child(%Membrane.AAC.Parser{out_encapsulation: :none, output_config: :esds})
+          end
+        )
+        |> get_child(:mp4)
+      end)
+
+    spec =
+      [
+        child(:mp4, Membrane.MP4.Muxer.ISOM)
+        |> child(:file_sink, %Membrane.File.Sink{location: "output.mp4"})
+      ] ++ streams_spec
+
+    {[spec: spec], state}
+  end
+
+  @impl true
+  def handle_element_end_of_stream(:file_sink, _pad, _ctx, state) do
+    {[terminate: :normal], state}
+  end
+
+  @impl true
+  def handle_element_end_of_stream(_element, _pad, _ctx, state) do
+    {[], state}
+  end
+end
+
+{:ok, receiving_supervisor, _pipeline} =
+  Membrane.Pipeline.start_link(ReceivingPipeline, port: 1234)
+
+Process.monitor(receiving_supervisor)
+
+receive do
+  {:DOWN, _ref, _type, ^receiving_supervisor, _reason} -> :ok
+end
