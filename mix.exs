1c00c75f8552abd0d0d46dcc0104f423e30f45e9
Łukasz Kita <lukasz.kita0@gmail.com>
2025-05-12 17:27:55 +0200

    Revoke old fixture

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Membrane.SRT.Mixfile do
   defp deps do
     [
       {:membrane_core, "~> 1.2.3"},
-      {:ex_libsrt, github: "Qizot/ex_libsrt"},
+      {:ex_libsrt, github: "membraneframework-labs/ex_libsrt", branch: "varsill/sleep_before_closing_socket"},
       {:membrane_mpeg_ts_plugin, github: "kim-company/membrane_mpeg_ts_plugin"},
       {:membrane_realtimer_plugin, "~> 0.10.1", only: :test},
       {:membrane_file_plugin, "~> 0.17.2", only: :test},
