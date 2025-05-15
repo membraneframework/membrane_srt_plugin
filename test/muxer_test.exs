e4be17b9da24577cccdcb2643f99e38c647c273c
Łukasz Kita <lukasz.kita0@gmail.com>
2025-05-15 16:05:44 +0200

    Refactor tests

--- test/muxer_test.exs
+++ test/muxer_test.exs
@@ -6,11 +6,63 @@ defmodule Membrane.SRT.MuxerTest do
   alias Membrane.H264.AUSplitter
   alias Membrane.MPEGTS.Muxer
 
+  @tag :sometag
   test "if the MPEG TS muxes H264 stream" do
-    # frames per second
-    fps = 30
-    input = File.read!("test/fixtures/bbb.h264")
+    input_path = "test/fixtures/bbb.h264"
     reference_path = "test/fixtures/reference.ts"
+
+    video_frames = get_video_frames(input_path, 30)
+    {payload1, state} = Muxer.new()
+    {payload2, state} = Muxer.register_track(:video, state)
+
+    {packets, _state} =
+      Enum.map_reduce(video_frames, state, fn {ts, type, frame}, state ->
+        Muxer.put_frame(frame, type, ts, ts, state)
+      end)
+
+    payload3 = Enum.join(packets)
+    payload = payload1 <> payload2 <> payload3
+    assert payload == File.read!(reference_path)
+  end
+
+  @tag :sometag
+  test "if the MPEG TS muxes H264 and AAC streams" do
+    input_audio_path = "test/fixtures/bbb.aac"
+    input_video_path = "test/fixtures/bbb.h264"
+    reference_path = "test/fixtures/reference_with_audio_and_video.ts"
+
+    audio_frames = get_audio_frames(input_audio_path, 1024, 44_100)
+    video_frames = get_video_frames(input_video_path, 25)
+
+    sorted_frames =
+      Enum.sort_by(video_frames ++ audio_frames, fn {ts, _type, _packet} -> ts end)
+
+    {payload1, state} = Muxer.new()
+    {payload2, state} = Muxer.register_track(:audio, state)
+    {payload3, state} = Muxer.register_track(:video, state)
+
+    {packets, _state} =
+      Enum.map_reduce(sorted_frames, state, fn {ts, type, frame}, state ->
+        Muxer.put_frame(frame, type, ts, ts, state)
+      end)
+
+    payload4 = Enum.join(packets)
+    payload = payload1 <> payload2 <> payload3 <> payload4
+    assert payload == File.read!(reference_path)
+  end
+
+  defp get_audio_frames(input_path, samples_per_frame, sampling_frequency) do
+    input = File.read!(input_path)
+    {aac_frames, _aac_parser} = Muxer.AACParser.parse(input, Muxer.AACParser.new())
+
+    Enum.with_index(aac_frames)
+    |> Enum.map(fn {aac_frame, i} ->
+      {i * 1000 * samples_per_frame / sampling_frequency, :audio, aac_frame}
+    end)
+  end
+
+  defp get_video_frames(input_path, fps) do
+    input = File.read!(input_path)
     {nalu_payloads, _spliter} = NALuSplitter.split(input, NALuSplitter.new())
     {nalus, _parser} = NALuParser.parse_nalus(nalu_payloads, NALuParser.new())
 
@@ -21,59 +73,6 @@ defmodule Membrane.SRT.MuxerTest do
     annexb_prefix = <<0, 0, 0, 1>>
     aus = Enum.map(aus, fn au -> Enum.map_join(au, &(annexb_prefix <> &1.payload)) end)
 
-    {payload1, state} = Muxer.new()
-    {payload2, state} = Muxer.register_track(:video, state)
-
-    {packets, _state} =
-      Enum.with_index(aus)
-      |> Enum.map_reduce(state, fn {au_payload, i}, state ->
-        Muxer.put_frame(au_payload, :video, i * 1000 / fps, i * 1000 / fps, state)
-      end)
-
-    payload3 = Enum.join(packets)
-    payload = payload1 <> payload2 <> payload3
-    assert payload == File.read!(reference_path)
-  end
-
-  test "if the MPEG TS muxes H264 and AAC streams" do
-    # frames per second
-    fps = 25
-    input_video = File.read!("test/fixtures/bbb.h264")
-    input_audio = File.read!("test/fixtures/bbb.aac")
-    reference_path = "test/fixtures/reference_with_audio_and_video.ts"
-    {nalu_payloads, _spliter} = NALuSplitter.split(input_video, NALuSplitter.new())
-    {nalus, _parser} = NALuParser.parse_nalus(nalu_payloads, NALuParser.new())
-
-    {aus, au_splitter} = AUSplitter.split(nalus, AUSplitter.new())
-    {[last_au], _au_splitter} = AUSplitter.split([], true, au_splitter)
-
-    aus = aus ++ [last_au]
-    annexb_prefix = <<0, 0, 0, 1>>
-    aus = Enum.map(aus, fn au -> Enum.map_join(au, &(annexb_prefix <> &1.payload)) end)
-
-    video_packets =
-      Enum.with_index(aus) |> Enum.map(fn {au, i} -> {i * 1000 / fps, :video, au} end)
-
-    {aac_frames, _aac_parser} = Muxer.AACParser.parse(input_audio, Muxer.AACParser.new())
-
-    audio_packets =
-      Enum.with_index(aac_frames)
-      |> Enum.map(fn {aac_frame, i} -> {i * 1000 * 1024 / 44_100, :audio, aac_frame} end)
-
-    sorted_packets =
-      Enum.sort_by(video_packets ++ audio_packets, fn {ts, _type, _packet} -> ts end)
-
-    {payload1, state} = Muxer.new()
-    {payload3, state} = Muxer.register_track(:audio, state)
-    {payload2, state} = Muxer.register_track(:video, state)
-
-    {packets, _state} =
-      Enum.map_reduce(sorted_packets, state, fn {ts, type, packet}, state ->
-        Muxer.put_frame(packet, type, ts, ts, state)
-      end)
-
-    payload4 = Enum.join(packets)
-    payload = payload1 <> payload2 <> payload3 <> payload4
-    assert payload == File.read!(reference_path)
+    Enum.with_index(aus) |> Enum.map(fn {au, i} -> {i * 1000 / fps, :video, au} end)
   end
 end
